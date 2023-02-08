31a3fdcf729f22f4ed28f941074ac48dd7aed459
batruonghuu <truonghuuba2888@gmail.com>
2023-02-08 23:29:19 +0700

    new

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import openpyxl
 import pandas as pd
 
 file_template_path = tkinter.filedialog.askopenfilename(title="Mở file template phiếu bảo dưỡng mẫu")
+file_groupttb_path = tkinter.filedialog.askopenfilename(title="Mở file group TTB")
+file_vlookup = tkinter.filedialog.askopenfilename(title="Mở file ánh xạ mã thiết bị")
 # ask directory
 
-file_groupttb_path = tkinter.filedialog.askopenfilename(title="Mở file group TTB")
-
 df_groupttb = pd.read_excel(file_groupttb_path)
+df_vlookup = pd.read_excel(file_vlookup)
 # create a pandas dataframe from file excel
 
 df_groupttb.fillna(method='ffill', inplace=True)
@@ -38,9 +39,14 @@ df_groupttb['Thời gian dự kiến'] = df_groupttb['Thời gian dự kiến'].
 # second: convert to strings,
 
 df_copy.drop_duplicates(subset='Thời gian dự kiến', keep='first', inplace=True)
+# remove the rows subsequently with same value in 'subset' column, only keep the first row
+
 df_copy['Thời gian dự kiến'] = pd.to_datetime(df_copy['Thời gian dự kiến'], errors='coerce',dayfirst=True)
 df_copy['Thời gian dự kiến'] = df_copy['Thời gian dự kiến'].dt.strftime('%Y-%m-%d')
 df_copy = df_copy[df_copy['Thời gian dự kiến'].notnull()]
+
+df_copy = df_copy.merge(df_vlookup,on='Tên trang thiết bị', how='left')
+# insert row from df_vloopup with matching based on column
 
 set_datetime = set(df_groupttb['Thời gian dự kiến'].unique())
 list_datetime = list(set_datetime)
@@ -49,23 +55,32 @@ print(list_datetime)
 df_groupttb.to_excel('dataframe.xlsx', index=False)
 df_copy.to_excel('dataframe_copy.xlsx', index=False)
 # Save to a excel
-ws = xl.Book(file_template_path).sheets.active
 def insert_new_row(brow,sheet,value1,value2):
     sheet.range(brow,1).api.EntireRow.Insert()
     # insert a new row with keeping format of previous row
+
     sheet.range(brow,2).value = value1
     sheet.range(brow,3).value = value2
+
     sheet.range(brow,1).autofit()
     # autofit row based on content
-# xxx = 8
-# df_copy = df_groupttb[df_groupttb['Thời gian dự kiến'] == list_datetime[0]]
-# for check in df_copy['Mã trang thiết bị']:
-#     insert_new_row(xxx,ws,check,'yyy')
-#     xxx = xxx + 1
+
+template_wb = openpyxl.load_workbook(file_template_path)
+originalsheet = template_wb['Original Sheet']
+
+for i in df_copy['Thời gian dự kiến']:
+    result1 = df_copy.loc[df_copy['Thời gian dự kiến'] == i, 'Tên trang thiết bị'].iloc[0]
+    result2 = df_copy.loc[df_copy['Thời gian dự kiến'] == i, 'Mã trang thiết bị new'].iloc[0]
+    # return the value of first column based on determine value from other column
+
+    duplicatesheet = template_wb.copy_worksheet(originalsheet)
+    duplicatesheet.title = i
+    template_wb.active = template_wb[i]
+template_wb.save('fileprint.xlsx')
 
 
 
-# insert_new_row(8,ws,'xxx','yyy')
+
 
 
 
